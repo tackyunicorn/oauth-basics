2c05c63e1b424927430c71a0402a72e263df3a6f
tackyunicorn <joshjoseph97@gmail.com>
2019-05-19 16:30:59 +0530

    :wrench: google strategy keys secured

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,12 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
+const keys = require('./keys')
 
 passport.use(
     new GoogleStrategy({
         // options for the strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }), () => {
         // passport callback function
     }
