1edb51c0044d6149c7b2dbaf733ce7ac43faeb42
tackyunicorn <joshjoseph97@gmail.com>
2019-05-19 15:17:36 +0530

    :arrow_right: auth routes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const authRoutes = require('./routes/auth-routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 // set up view engine
 app.set('view engine', 'ejs')
+
+// set up routes
+app.use('/auth', authRoutes)
 
 // create home route
 app.get('/', (req, res) => {
