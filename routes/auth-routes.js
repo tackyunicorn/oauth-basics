1fe4c0f090e726da39541f93dce84dd428317860
tackyunicorn <joshjoseph97@gmail.com>
2019-05-20 16:24:52 +0530

    :lock: securing profile routes via middleware redirection

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -19,7 +19,8 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.send(req.user)
+    // res.send(req.user)
+    res.redirect('/profile/')
 })
 
 module.exports = router
